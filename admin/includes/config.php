4a232c1e3e2fffd6a099de3df3ab0e631dc30644
Xuanchen97 <jasonliu97424@gmail.com>
2020-12-23 23:08:52 +0800

    updated db credentials

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -2,10 +2,10 @@
 
 <?php 
 // DB credentials.
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','');
-define('DB_NAME','shiqi');
+define('DB_HOST', 'jhdjjtqo9w5bzq2t.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
+define('DB_USER', 'o1yjtwpo7v3e7ekv');
+define('DB_PASS', 'mlyvhljfy0tdsw0y');
+define('DB_NAME', 'kzt4tyivwvxhpaxb');
 // Establish database connection.
 try
 {
--- includes/config.php
+++ includes/config.php
@@ -1,9 +1,9 @@
 <?php
 // DB credentials.
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'shiqi');
+define('DB_HOST', 'jhdjjtqo9w5bzq2t.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
+define('DB_USER', 'o1yjtwpo7v3e7ekv');
+define('DB_PASS', 'mlyvhljfy0tdsw0y');
+define('DB_NAME', 'kzt4tyivwvxhpaxb');
 // Establish database connection.
 try {
     $dbh = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
