26fa7fb5fbcd94bab14a1e10e4f3e36e5b698aab
Xuanchen97 <jasonliu97424@gmail.com>
2020-12-31 13:42:50 +0800

    add composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,60 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '95dd5716dd4441cf7baf0e6bf0a0d23870b2b757',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '95dd5716dd4441cf7baf0e6bf0a0d23870b2b757',
+    ),
+    'symfony/polyfill-ctype' => 
+    array (
+      'pretty_version' => 'v1.20.0',
+      'version' => '1.20.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f4ba089a5b6366e453971d3aad5fe8e897b37f41',
+    ),
+    'symfony/polyfill-mbstring' => 
+    array (
+      'pretty_version' => 'v1.20.0',
+      'version' => '1.20.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '39d483bdf39be819deabf04ec872eb0b2410b531',
+    ),
+    'twig/extensions' => 
+    array (
+      'pretty_version' => 'v1.5.4',
+      'version' => '1.5.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '57873c8b0c1be51caa47df2cdb824490beb16202',
+    ),
+    'twig/twig' => 
+    array (
+      'pretty_version' => 'v2.14.1',
+      'version' => '2.14.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5eb9ac5dfdd20c3f59495c22841adc5da980d312',
+    ),
+  ),
+);
